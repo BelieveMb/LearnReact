ccbdc06cf79b27d19e01c837c704d4dae57984f7
Dev Unbelievable <mbolokobelieve@gmail.com>
2023-11-14 14:59:04 +0100

    mettre à jour le squares

--- start-with-react/src/App.js
+++ start-with-react/src/App.js
@@ -1,19 +1,36 @@
 import { useState } from "react";
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
-  function Square() {
-    const [value, setValue] = useState(null);
-    //valuestocke la valeur et setValueest une fonction qui peut être utilisée pour modifier la valeur
-    function handleClick() {
-      setValue("X");
-      // En appelant cette setfonction à partir d'un onClickgestionnaire, vous demandez à React de restituer cette fonction Squarechaque fois que vous <button>cliquez dessus.
+
+  function handleClick(i) {
+    //pour mettre à jour le squarestableau contenant l'état de votre carte :
+    if (squares[i]) {
+      return;
     }
-    return (
-      <button className="square" onClick={handleClick}>
-        {value}
-      </button>
-    );
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+  function Square({value, onSquareClick}) {
+    return <button className="square" onClick={onSquareClick}>{value}</button>;
+    // const [value, setValue] = useState(null);
+    // //valuestocke la valeur et setValueest une fonction qui peut être utilisée pour modifier la valeur
+    // function handleClick() {
+    //   setValue("X");
+    //   // En appelant cette setfonction à partir d'un onClickgestionnaire, vous demandez à React de restituer cette fonction Squarechaque fois que vous <button>cliquez dessus.
+    // }
+    // return (
+    //   <button className="square" onClick={handleClick}>
+    //     {value}
+    //   </button>
+    // );
   }
   // En effet, le Boardcomposant n'a pas encore transmis l' valueaccessoire à
   // chaque Squarecomposant qu'il restitue.
@@ -21,20 +38,21 @@ export default function Board() {
   return (
     <div className="game">
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)}  />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+      <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
+      {/* Notez la nouvelle () =>syntaxe. Voici () => handleClick(0)une fonction fléchée, qui est un moyen plus court de définir des fonctions. Lorsque le carré est cliqué, le code après la =>« flèche » s’exécutera, appelant handleClick(0). */}
     </div>
   );
 }
