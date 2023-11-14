702b2a265a08a4daf52a81545633505c25059de7
Dev Unbelievable <mbolokobelieve@gmail.com>
2023-11-14 18:39:36 +0100

    end game square

--- start-with-react/src/App.js
+++ start-with-react/src/App.js
@@ -6,7 +6,8 @@ export default function Board() {
 
   function handleClick(i) {
     //pour mettre à jour le squarestableau contenant l'état de votre carte :
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
+      // Vous appellerez calculateWinner(squares)la fonction Boarddu composant handleClickpour vérifier si un joueur a gagné. Vous pouvez effectuer cette vérification en même temps que vous vérifiez si un utilisateur a cliqué sur un carré comportant déjà un Xou et O. 
       return;
     }
     const nextSquares = squares.slice();
@@ -34,9 +35,20 @@ export default function Board() {
   }
   // En effet, le Boardcomposant n'a pas encore transmis l' valueaccessoire à
   // chaque Squarecomposant qu'il restitue.
-  // Pour résoudre ce problème, vous ajouterez le valueprop à chaque Squarecomposant rendu par le Board
+  // Pour résoudre ce problème, vous ajouterez le valueprop à chaque
+  // Squarecomposant rendu par le Board
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
   return (
     <div className="game">
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)}  />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -55,4 +67,26 @@ export default function Board() {
       {/* Notez la nouvelle () =>syntaxe. Voici () => handleClick(0)une fonction fléchée, qui est un moyen plus court de définir des fonctions. Lorsque le carré est cliqué, le code après la =>« flèche » s’exécutera, appelant handleClick(0). */}
     </div>
   );
+}
+
+
+//calcule rle winner
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
